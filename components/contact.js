e573d963f89f6ca581f942ab833e2c75bc0ee1cd
InmortalKaktus <inmortalkaktus@hotmail.com>
2022-05-11 11:16:43 +0200

    In animation for contact form

--- components/contact.js
+++ components/contact.js
@@ -1,19 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from '../styles/Contact.module.css'
-import { motion } from 'framer-motion'
+import { useAnimation, motion } from 'framer-motion'
+import { useInView } from "react-intersection-observer";
 import { TiHeartFullOutline } from "react-icons/ti"
 
 const Contact = () => {
     const [subject, setSubject] = useState('')
     const [message, setMessage] = useState('')
 
+    const [ref, inView] = useInView({ "threshold": 0.3 });
+    const [show, setShow] = useState(false)
+
+    const variants = {
+        show: { opacity: 1, transition: { duration: .75 } },
+        hide: { opacity: 0 }
+    };
+
+    const itemVariant = (item, yVariation) => {
+        return {
+            show: { opacity: 1, y: "0%", transition: { duration: .75, delay: item * 0.075 } },
+            hide: { opacity: 0, y: yVariation },
+        }
+    }
+
+    useEffect(() => {
+        if (inView && !show) {
+            setShow(true)
+        } else if (!inView && show) {
+            setShow(false)
+        }
+    }, [inView]);
+
     const getHref = () => {
         return `mailto:alejandroolivaresmompo@hotmail.com?subject=${subject ?? ''}&body=${message ?? ''}`
     }
 
     return (
-        <section className={styles["container"]}>
-            <motion.span className={styles["contact-me"]}><h1>CONTACT ME</h1><TiHeartFullOutline/></motion.span>
+        <motion.section ref={ref} className={styles["container"]}>
+            <motion.span 
+                className={styles["contact-me"]}
+                variants={ itemVariant(1, "40%") }
+                initial={ "hide" }
+                animate={ show ? "show" : "hide" }
+            >
+                <h1>CONTACT ME</h1><TiHeartFullOutline/>
+            </motion.span>
             <form className={styles.form}>
                 <motion.input
                     onChange={ e => setSubject(e.target.value)}
@@ -23,6 +54,9 @@ const Contact = () => {
                     placeholder='Subject'
                     className={styles.subject}
                     tabIndex="1"
+                    variants={ itemVariant(2, "40%") }
+                    initial={ "hide" }
+                    animate={ show ? "show" : "hide" }
                 />
 
                 <motion.textarea
@@ -33,11 +67,20 @@ const Contact = () => {
                     name="message"
                     rows={7}
                     tabIndex="2"
+                    variants={ itemVariant(3, "15%") }
+                    initial={ "hide" }
+                    animate={ show ? "show" : "hide" }
                 />
 
-                <motion.a target="_blank" href={ getHref() } tabIndex="3" className={ styles.send }>Send</motion.a>
+                <motion.a target="_blank" href={ getHref() } tabIndex="3" className={ styles.send }
+                    variants={ itemVariant(4, "40%") }
+                    initial={ "hide" }
+                    animate={ show ? "show" : "hide" }
+                >
+                    Send
+                </motion.a>
             </form>
-        </section>
+        </motion.section>
     )
 }
 
