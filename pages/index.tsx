89a1f76e2dd1fa85b9962fc4d6875ecc703efbe6
Alejandro Olivares Mompó <alejandroolivaresmompo@hotmail.com>
2023-01-16 19:35:41 +0100

    refactor: :recycle: Set up new environment for portfolio-v2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,15 @@
-import React, { useState } from 'react'
-import { AnimatePresence, AnimateSharedLayout } from 'framer-motion'
+import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Content from '../components/Home/Content/Content'
-import Navbar from '../components/Home/Menus/Navbar/Navbar'
-import SEO from '../components/Home/SEO/SEO'
-import styles from '../styles/Home.module.css'
-import Loader from '../components/Home/Loader/Loader'
 
-export default function Home() {
-    const [ loading, setLoading ] = useState(true)
-
-    return (
-        <>
-            <SEO />
-            <AnimatePresence>
-                {
-                    loading ?
-                        <Loader setLoading={ setLoading } />
-                    :
-                    <>
-                        <Navbar />
-                        <div className={styles.container}>
-                            <Content />
-                        </div>
-                    </>
-                }
-            </AnimatePresence>
-        </>
-    )
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Alejandro Olivares — Frontend Developer</title>
+      </Head>
+    </div>
+  )
 }
+
+export default Home
